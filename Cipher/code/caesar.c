2e7ae4a8af1a2c66d055b232927529d833fb7495
Victor Rakotondranoro <vrakotondranoro@luc.edu>
2021-10-16 21:19:11 +0000

    fixed a merging issue

--- Cipher/code/caesar.c
+++ Cipher/code/caesar.c
@@ -11,7 +11,6 @@
 // empty a file
 void empty_all_capital_file()
 {
-<<<<<<< HEAD
   fclose(fopen("../all_capital.txt", "w"));
 }
 
@@ -39,23 +38,5 @@ int main(){
   empty_all_capital_file();
   uppercase_to_file();
 }
-=======
-  char * current_line;
-  current_line = malloc(MAX);
-  // read file and store line into current_line string
-  char current_char;
-  int count = 0;
-  while ((current_char = read_char_from_file(count)) != '\n')
-  {
-    *(current_line + count) = current_char;
-    count++;
-  }
- 
-  // convert current_line to all capital letter
-  current_line = convert_to_uppercase(current_line);
-  printf("%s\n", current_line);
-  // write current_line to all_capital.txt
-  // for(int i = 0; i < strlen(current_line); i++)
-  //   write_to_text_file(current_line[i], "all_capital.txt");
-}
->>>>>>> a54d519a81031033c19cd1a1cecfc6353fdf5155
+
+  
